b68e2cf23e224bd8b85841b29dee144b8ab0fe81
Mayank Pabbi <mayankpabbi@gmail.com>
2020-06-08 18:40:18 +0530

    Updates

---
+++ precache-manifest.9422a4b38a8f152e30e9fe82caac4b83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "80f5ab97df7a14df9b2a",
+    "url": "/dice/static/css/main.6ec4bdbc.chunk.css"
+  },
+  {
+    "revision": "80f5ab97df7a14df9b2a",
+    "url": "/dice/static/js/main.80f5ab97.chunk.js"
+  },
+  {
+    "revision": "499e7ae06760a11dc63e",
+    "url": "/dice/static/js/1.499e7ae0.chunk.js"
+  },
+  {
+    "revision": "8bc220e9f490c2582be6",
+    "url": "/dice/static/js/runtime~main.8bc220e9.js"
+  },
+  {
+    "revision": "09756eeed13b711e3544969707a0e077",
+    "url": "/dice/index.html"
+  }
+];
